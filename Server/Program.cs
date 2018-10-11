6de6d5ec23b52e4e8c12f54c18facdea63530da1
Jens Bo Pedersen <jensp25@gmail.com>
2018-10-11 14:18:11 +0200

    Helper

--- Server/Program.cs
+++ Server/Program.cs
@@ -42,4 +42,70 @@ namespace EchoServer
         }
 
     }
+        /**********************************************************
+         * 
+         *  Helper Methods
+         * 
+        **********************************************************/
+
+        private static string UnixTimestamp()
+        {
+            return DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+        }
+
+        private static TcpClient Connect()
+        {
+            IPEndPoint ipLocalEndPoint = new IPEndPoint(IPAddress.Loopback, Port);
+            var client = new TcpClient();
+            client.Connect(ipLocalEndPoint);
+            return client;
+        }
+
+    }
+
+    /**********************************************************
+    * 
+    *  Helper Clases
+    * 
+    **********************************************************/
+
+    public static class Util
+    {
+        public static string ToJson(this object data)
+        {
+            return JsonConvert.SerializeObject(data,
+            new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+        }
+
+        public static T FromJson<T>(this string element)
+        {
+            return JsonConvert.DeserializeObject<T>(element);
+        }
+
+        public static void SendRequest(this TcpClient client, string request)
+        {
+            var msg = Encoding.UTF8.GetBytes(request);
+            client.GetStream().Write(msg, 0, msg.Length);
+        }
+
+        public static Response ReadResponse(this TcpClient client)
+        {
+            var strm = client.GetStream();
+            //strm.ReadTimeout = 250;
+            byte[] resp = new byte[2048];
+            using (var memStream = new MemoryStream())
+            {
+                int bytesread = 0;
+                do
+                {
+                    bytesread = strm.Read(resp, 0, resp.Length);
+                    memStream.Write(resp, 0, bytesread);
+
+                } while (bytesread == 2048);
+                
+                var responseData = Encoding.UTF8.GetString(memStream.ToArray());
+                return JsonConvert.DeserializeObject<Response>(responseData);
+            }
+        }
+    }
 }
